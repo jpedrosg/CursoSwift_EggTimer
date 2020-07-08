9b390c7cbf5fed3cb920f77d417268d62b85a914
João Pedro Giarrante <joao.giarrante@valemobi.com.br>
2020-07-08 01:46:52 -0300

    first / last commit

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,6 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
     
-
-
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    @IBOutlet weak var lblTitle: UILabel!
+    
+    let eggType: [String:Int] = ["Soft" : 5 , "Medium" : 7 , "Hard" : 12]
+    var eggTimer:Timer? = nil
+    
+    @IBAction func eggClicked(_ sender: UIButton) {
+        if eggTimer == nil {
+            
+            let eggTypeSelected = sender.currentTitle!
+            self.lblTitle.text = String(format:"Selected: %@", eggTypeSelected)
+            
+            let timeSelected = eggType[eggTypeSelected]!
+            var timePassed = 0
+            
+            self.eggTimer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { timer in
+                
+                timePassed += 1
+                self.lblTitle.text = String(format:"%d seconds..", timePassed)
+                self.progressBar.setProgress(Float(timePassed)/Float(timeSelected), animated: true)
+                if timePassed == timeSelected {
+                    print("\nHell yeah! Your ", eggTypeSelected, " egg is ready!")
+                    self.eggTimer?.invalidate()
+                    self.eggTimer = nil
+                }
+            }
+        }
+        
+    }
+    
 }
